a961082bd9a52b429b3ce98381aff647eec4aa30
Umberto Laghi <ul2509@gmail.com>
2024-04-15 16:18:25 +0200

    added trdb_branch_map interface

---
+++ rtl/trdb_branch_map.sv
@@ -0,0 +1,21 @@
+/* BRANCH MAP */
+/*
+it keeps track of taken and non taken branches
+*/
+
+module trdb_branch_map
+(
+    input logic clk_i,
+    input logic rst_ni,
+
+    input logic valid_i,
+    input logic branch_taken_i,
+    input logic flush_i,
+
+    output logic [:0] map_o,
+    output logic [:0] branches_o,
+    output logic is_full_o,
+    output logic is_empty_o
+);
+    
+endmodule
