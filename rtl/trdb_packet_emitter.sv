d9e0cb63db18bae84639e7a1f03e09dd56b2ea52
Umberto Laghi <ul2509@gmail.com>
2024-04-16 15:12:25 +0200

    added input signals and comments

--- rtl/trdb_packet_emitter.sv
+++ rtl/trdb_packet_emitter.sv
@@ -26,18 +26,21 @@ module trdb_packet_emitter
 
     // nc (next cycle) signals
 
+
     // format 3 subformat 0 specific signals
     input logic is_branch_i,
     input logic is_branch_taken_i,
     input logic [PRIVLEN:0] priv_i,
+    input logic [:0] time_i,    // optional
+    input logic [:0] context_i, // optional
     input logic [XLEN-1:0] iaddr_i,
 
     // format 3 subformat 1 specific signals
     //input logic is_branch_i,
     //input logic is_branch_taken_i,
     //input logic [PRIVLEN:0] priv_i,
-    input logic [:0] time_i,
-    input logic [:0] context_i,
+    //input logic [:0] time_i,
+    //input logic [:0] context_i,
     input logic [CAUSELEN:0] ecause_i,
     input logic interrupt_i,
     input logic [XLEN-1:0] trap_handler_address_i,
@@ -53,36 +56,84 @@ module trdb_packet_emitter
 
     // format 3 subformat 3 specific signals
     input logic ienable_i, // trace encoder enabled
-    input logic [:0] encoder_mode_i, // implementation specific, right now only branch trace supported (value==0)
+    input logic encoder_mode_i, // implementation specific, right now only branch trace supported (value==0)
     input logic [1:0] qual_status_i,
-    input logic [:0] ioptions_i, // meaning to be understood
+    //input logic [:0] ioptions_i, // implementation specific
+    // doesn't require an input, it must be created from other inputs
+    /*  Run-time configuration bits for INSTRUCTION trace.
+        Examples:
+            - sequentially inferred jump: don't report the targets of sequentially inferable jumps
+            - implicit return: don't report the targets of sequentially inferrable jumps
+            - implicit exception: don't report function return addresses
+            - branch prediction: branch predictor enabled (not supported in snitch)
+            - jump target cache: enabled JTC (not supported in snitch)
+            - full address: always output full addresses
+
+        it requires info from the CSRs storing the values
+    */
+    //input logic seq_inferred_jump_i, // to implement
+    input logic trace_implicit_ret_i,
+    //input logic trace_implicit_exc_i, // to implement
+    //input logic trace_branch_prediction_i, // not supported by snitch, hardwired to 0 (?)
+    //input logic jump_target_cache_i, // not supported by snitch, hardwired to 0 (?)
+    input logic trace_full_addr_i, // always output full address
+
+
     input logic denable_i, // DATA trace enabled
     input logic dloss_i, // one or more DATA trace packets lost
-    input logic [:0] doptions_i, // implementation specific - to be understood
+    input logic [:0] doptions_i, // implementation specific
+    /* */
 
     // format 2 specific signals
-    // all must be computed using other info
+    //input logic [XLEN-1:0] iaddr_i,
+    input logic lc_updiscon_i,
+    input logic irreport_i,
+    input logic irdepth_i,
+    // the last 2 signals are provided by the priority module
+
 
     // format 1 specific signals
+    /*  this format exists in two modes:
+            - address, branch map
+            - NO address, branch maps
+        
+        Their generation depends on the value of branches:
+            - 0: no need for address
+            - >0: address required
+    */
+    input logic [:0] branches_i, // in Robert implementation is called branch_cnt
     input logic [:0] branch_map_i,
     //input logic [XLEN-1:0] iaddr_i,
+    //input logic lc_updiscon_i,
+    //input logic irreport_i,
+    //input logic irdepth_i,
+    
 
     // format 0 specific signals
+    /*  This format can have two possible subformats:
+            - subformat 0: number of correctly predicted branches
+            - subformat 1: jump target cache index
+
+    Since snitch does NOT support any of them,
+    this format of packet is not necessary
+
+    */
     //input logic [:0] branch_map_i,
 
 
     // outputs
-    output logic [PTYPELEN:0]packet_type_o,
+    output logic [PTYPELEN:0]packet_type_o, // {packet_format, packet_subformat}
     output logic [PLEN:0] packet_length_o, // in bytes
-    output logic [PAYLOADLEN:0] packet_payload_o
+    output logic [PAYLOADLEN:0] packet_payload_o,
 
-    /* TO DO:
-    outputs to perform reset resync counter
-    and update/reset branch map
-
+    /*outputs to perform reset resync counter
+    and update/reset branch map.
     Question:   it should be done in this module or in
-                the one choosing the packet format
-    */
+                the one choosing the packet format*/
+    output logic branch_map_flush_o, // flushes the branch map
+    output logic manual_rst_o,  // not final
+                                // understand how the Robert tracer does that
+
 );
     
 endmodule
