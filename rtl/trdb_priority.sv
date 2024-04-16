7f0b7781f0db0aeded71ee6363125123f9e2b09c
Umberto Laghi <ul2509@gmail.com>
2024-04-16 10:30:58 +0200

    edited interface

--- rtl/trdb_priority.sv
+++ rtl/trdb_priority.sv
@@ -17,14 +17,49 @@ module trdb_priority (
     /*TO DO: determine width of signals, not all are logic*/
 
     // lc (last cycle) signals
-    input logic lc_exception_i,
+    input logic lc_exception_i, //
     input logic lc_updiscon_i, // updsicon == uninferable PC discontinuity
 
     // tc (this cycle) signals
-    input logic tc_qualified_i,
-    input logic tc_is_branch_i,
+    input logic tc_qualified_i, //
+    input logic tc_is_branch_i, //
     input logic tc_exception_i,
-    input logic tc_reported_i, // what's the meaning? refer to page 51 of spec
+
+    //input logic tc_exc_only_i, // not necessary a special input signal
+    /* Exc_only meaning:
+        Exception or interrupt signalled without
+        simultaneous retirement.
+
+        This means that in this cycle only the 
+        exception signal is 1 and the number of 
+        retired instructions is 0.
+    */
+    input logic tc_retired_i, // istr retired in tc
+    // used to create tc_exc_only and er_n signal
+
+
+    //input logic tc_reported_i, // what's the meaning? refer to page 51 of spec
+    /* Reported meaning:
+        The "exception previous" (i.e. the lc_exception)
+        was reported in a packet with thaddr = 0 on the
+        cycle it occured, beacause it was preceded by an
+        updiscon OR immediately followed by another exception
+    
+        This means I need to consider the value of the previous
+        packet and if its thaddr value was 0.
+        The thaddr value appears only in 3.1 format packets.
+        I need a signal like: "lc_thaddr" (last cycle thaddr),
+        the value of this signal is the same as the parameter
+        in the packet.    
+    
+        // ask Simone if the reasoning is correct
+    */
+    
+    //input logic lc_thaddr_i,
+    /*  no need to have it as input, because it can be kept as
+        a value inside the module*/
+
+
     input logic tc_first_qualified_i,
     input logic tc_privchange_i,
     input logic tc_precise_context_change_i,
@@ -32,23 +67,73 @@ module trdb_priority (
     input logic tc_context_change_w_discontinuity_i, // understand the meaning
     input logic tc_exception_sync_i, // resync timer expired
     input logic tc_branch_map_empty_i,
-    input logic tc_er_n_i, // simultaneous exception and retirement, maybe split in more signals?
+    
+    //input logic tc_er_n_i, // no need for a specific input signal
+    /*er_n: exception and retirement in the same cycle*/
+
     input logic tc_branch_map_full_i,
+    /* rpt_br:  report branches due to full branch_map
+                or misprediction
+    */
+    input logic tc_branch_misprediction_i, // signal to determine misprediction
+    // determine which module generates this signal (branch_map?)
+
     input logic tc_imprecise_context_change_i, // understand
     // understand the meaning of branches and pbc in graph at page 53
+    /*
+    ppccd:  priv has changed OR context has changed
+            and it needs to be reported precisely or
+            treated as an updiscon
+    */
+
+    // format 3 subformat 3 - NOT shown in graph
+    input logic tc_enc_enabled_i,
+    input logic tc_enc_disabled_i,
+    input logic tc_final_instr_traced_i,
+    input logic tc_packets_lost_i,
+
 
     // nc (next cycle) signals
     input logic nc_exception_i,
-    input logic nc_privchange_i,
-    input logic nc_precise_context_change_i,
-    input logic nc_context_change_w_discontinuity_i,
-    input logic nc_branch_map_empty_i,
-    input logic nc_qualified_i,
+    input logic nc_privchange_i, //
+    input logic nc_precise_context_change_i, //
+    input logic nc_context_change_w_discontinuity_i, //
+    input logic nc_branch_map_empty_i, //
+    input logic nc_qualified_i, //
 
-    // to do: outputs
+    input logic nc_retired_i, // used w/nc_exception for signal nc_exc_only
+
+    /* 
+    ppccd_br:   priv has changed OR context has changed
+                and it needs to be reported precisely or
+                treated as an updiscon AND branch map is
+                not empty
+    no need for a dedicated input signal
+    Signals required already present
+    */
+
+
+    /* Precise/imprecise context report: refer to page 22 of the spec
+        Precise:    required the address of the first instr retired
+                    AND the new context.
+                    Unreported branches must be reported first.
+
+        Imprecise:  required the new context value
+    
+    */
     output logic            valid_o,
     output trdb_format_e    packet_format_o,
-    output trdb_subformat_e packet_subformat_o);
+    output trdb_subformat_e packet_subformat_o,
+    );
+
+    // refer to question at line 83 of packet_emitter
+
+
+
+
+
+
+
 
 
 
@@ -129,36 +214,6 @@ module trdb_priority (
             end
         end
     end
-
-            /* format 3 packet */
-            // subformat 0
-
-
-            // subformat 1
-
-
-            // subformat 2
-
-
-            // subformat 3
-
-
-
-
-            // format 2 packet
-
-
-
-
-
-
-            // format 1 packet
-
-
-
-
-
-            // format 0 packet
 
 
 endmodule
