422cdc9d6dbceb654da9be87bf1db7a4522cc892
Umberto Laghi <ul2509@gmail.com>
2024-04-17 17:40:38 +0200

    added logic and trigger unit input

--- rtl/trdb_filter.sv
+++ rtl/trdb_filter.sv
@@ -15,6 +15,14 @@ module trdb_filter
     input logic trace_activated_i,
     output logic trace_req_deactivate_o, // writes to register
 
+    // trigger unit
+    // this value influences the trace enabled signal
+    input logic [3:0] trigger_i,
+    /*  possible values:
+            - 2: trace-on,
+            - 3: trace-off
+    */
+
     /*
     // consider/ignore filters
     input logic apply_filters_i,
@@ -37,6 +45,38 @@ module trdb_filter
     */
     output logic trace_qualified_o
 );
+
+    logic trace_activated;
+    logic trigger_trace_on;
+    logic trigger_trace_off;
+
+    always_comb begin: trigger_check
+        // init signals
+        trigger_trace_off = 0;
+        trigger_trace_on = 0;
+
+        if(trigger_i == 2) begin
+            trigger_trace_on = 1;
+            trigger_trace_off = 0;
+        end
+        else if(trigger_i == 3) begin
+            trigger_trace_off = 1;
+            trigger_trace_on = 0;
+        end
+    end
+
+    
+
+    always_comb begin
+        trace_qualified_o = '0;
+        // tracing
+        if(!apply_filters_i)
+            trace_qualified_o = trace_activated_i;
+        else
+            trace_qualified_o = trace_activated_i;
+                                //&& (trace_range_event_i && ip_in_range) && priv_matching;
+    end
+
 
 
 
