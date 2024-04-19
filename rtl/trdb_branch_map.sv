3752a5337623496244c5e6da675a55ca6b4bc4f0
Umberto Laghi <ul2509@gmail.com>
2024-04-19 18:28:45 +0200

    added output to signal when a branch is mispredicted

--- rtl/trdb_branch_map.sv
+++ rtl/trdb_branch_map.sv
@@ -20,6 +20,7 @@ module trdb_branch_map
     output logic [:0] map_o, // array of branch taken and not 
     output logic [:0] branches_o, // number of branches stored
     output logic [:0] pbc_o, // correctly predicted branch count, not supported by snitch
+    output logic misprediction_o, // tells if the prediction was right, not supported by snitch
     output logic is_full_o,
     output logic is_empty_o
 );
