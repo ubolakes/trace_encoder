3d90368a7ac2d0c2653ad2e459b7255de3cd21a3
Umberto Laghi <ul2509@gmail.com>
2024-04-22 10:17:56 +0200

    edited logic and added signals

--- rtl/trdb_resync_counter.sv
+++ rtl/trdb_resync_counter.sv
@@ -13,24 +13,34 @@ module trdb_counter_emitter
 
     input logic trace_enabled_i, // it counts with the tracer
     input logic packet_emitted_i,
+    input logic resync_rst_i,
 
     output logic resync_max_o);
 
     logic [31:0] counter; // placeholder value
     logic enabled;
+    logic count_enabled;
+
+    assign count_enabled = trace_enabled_i && enabled;
 
     always_ff @( posedge clk_i, negedge rst_ni ) begin: counter
         if(~rst_ni) begin
             counter <= '0;
+            enabled <= '1; // enabled by default
         end else begin
             if(counter == MAX_VALUE) begin
                 resync_max_o <= '1;
-                counter <= '0;
-            end else if(PACKET_MODE && trace_enabled_i) begin
+                //counter <= '0; // reset to zero is done by priority module
+                enabled <= '0; // waits for a resync reset to count
+            end else if(PACKET_MODE && count_enabled) begin
                 if(packet_emitted_i)
                     counter++;
-            end else if(CYCLE_MODE && trace_enabled_i) begin
+            end else if(CYCLE_MODE && count_enabled) begin
                 counter++;
+            end else if(resync_rst_i) begin
+                resync_max_o <= '0;
+                counter <= '0;
+                enabled <= '1;
             end
         end
     end
