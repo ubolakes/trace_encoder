9776544afca0c4677f1169859631068d2d54fd48
Umberto Laghi <ul2509@gmail.com>
2024-05-10 12:26:53 +0200

    minor editing

--- rtl/trdb_itype_detector.sv
+++ rtl/trdb_itype_detector.sv
@@ -8,26 +8,16 @@ import trdb_pkg::*;
 module trdb_itype_detector
 (
     input logic             valid_i,
-    input logic [XLEN-1:0]  inst_data_i,
+    input logic [XLEN-1:0]  tc_inst_data_i,
     input logic             compressed_i,
     input logic [PC_LEN:0]  tc_iaddr_i,
     input logic [PC_LEN:0]  nc_iaddr_i,
+    //input logic             implicit_return_i, // non mandatory
 
     // outputs
     output logic            is_branch_o,
     output logic            is_branch_taken_o,
-    // not really necessary
-    /*output logic really_c_jalr_o,
-    output logic really_c_jr_o,
-    output logic is_jump_o,
-    output logic is_priv_ret_o,
-    output logic not_ret_o,
-    output logic is_ret_o,
-    output logic is_c_ret_o,*/
-    output logic            updiscon_o,
-
-
-
+    output logic            updiscon_o // it's considered tc, because the inputs are all tc
 );
     logic really_c_jr_o;
     logic is_jump_o;
@@ -38,42 +28,41 @@ module trdb_itype_detector
 
     always_comb begin: is_branch
         is_branch_o
-            = ((inst_data_i & MASK_BEQ)      == MATCH_BEQ) ||
-              ((inst_data_i & MASK_BNE)      == MATCH_BNE) ||
-              ((inst_data_i & MASK_BLT)      == MATCH_BLT) ||
-              ((inst_data_i & MASK_BGE)      == MATCH_BGE) ||
-              ((inst_data_i & MASK_BLTU)     == MATCH_BLTU) ||
-              ((inst_data_i & MASK_BGEU)     == MATCH_BGEU) ||
-              ((inst_data_i & MASK_P_BNEIMM) == MATCH_P_BNEIMM) ||
-              ((inst_data_i & MASK_P_BEQIMM) == MATCH_P_BEQIMM) ||
-              ((inst_data_i & MASK_C_BEQZ)   == MATCH_C_BEQZ) ||
-              ((inst_data_i & MASK_C_BNEZ)   == MATCH_C_BNEZ);
+            = ((tc_inst_data_i & MASK_BEQ)      == MATCH_BEQ) ||
+              ((tc_inst_data_i & MASK_BNE)      == MATCH_BNE) ||
+              ((tc_inst_data_i & MASK_BLT)      == MATCH_BLT) ||
+              ((tc_inst_data_i & MASK_BGE)      == MATCH_BGE) ||
+              ((tc_inst_data_i & MASK_BLTU)     == MATCH_BLTU) ||
+              ((tc_inst_data_i & MASK_BGEU)     == MATCH_BGEU) ||
+              ((tc_inst_data_i & MASK_P_BNEIMM) == MATCH_P_BNEIMM) ||
+              ((tc_inst_data_i & MASK_P_BEQIMM) == MATCH_P_BEQIMM) ||
+              ((tc_inst_data_i & MASK_C_BEQZ)   == MATCH_C_BEQZ) ||
+              ((tc_inst_data_i & MASK_C_BNEZ)   == MATCH_C_BNEZ);
     end
 
     always_comb begin: is_discontinuity
+        really_c_jalr = ((tc_inst_data_i & MASK_C_JALR) == MATCH_C_JALR)
+                        && ((tc_inst_data_i & MASK_RD) != 0);
 
-        really_c_jalr_o = ((inst_data_i & MASK_C_JALR) == MATCH_C_JALR)
-                        && ((inst_data_i & MASK_RD) != 0);
+        really_c_jr = ((tc_inst_data_i & MASK_C_JR) == MATCH_C_JR)
+                      && ((tc_inst_data_i & MASK_RD) != 0);
 
-        really_c_jr_o = ((inst_data_i & MASK_C_JR) == MATCH_C_JR)
-                      && ((inst_data_i & MASK_RD) != 0);
-
-        is_jump_o = ((inst_data_i & MASK_JALR) == MATCH_JALR) ||
+        is_jump = ((tc_inst_data_i & MASK_JALR) == MATCH_JALR) ||
                   really_c_jalr || really_c_jr;
 
-        is_priv_ret_o = ((inst_data_i & MASK_MRET) == MATCH_MRET) ||
-                  ((inst_data_i & MASK_SRET) == MATCH_SRET) ||
-                  ((inst_data_i & MASK_URET) == MATCH_URET);
+        is_priv_ret = ((tc_inst_data_i & MASK_MRET) == MATCH_MRET) ||
+                  ((tc_inst_data_i & MASK_SRET) == MATCH_SRET) ||
+                  ((tc_inst_data_i & MASK_URET) == MATCH_URET);
 
         // allows us to mark ret's as not being discontinuities, if we want
-        not_ret_o = '1;
-        is_ret_o = ((inst_data_i & (MASK_JALR | MASK_RD | MASK_RS1 | MASK_IMM)) ==
+        not_ret = '1;
+        is_ret = ((tc_inst_data_i & (MASK_JALR | MASK_RD | MASK_RS1 | MASK_IMM)) ==
                  (MATCH_JALR | (X_RA << OP_SH_RS1)));
-        is_c_ret_o = (inst_data_i & (MASK_C_JR | MASK_RD)) ==
+        is_c_ret = (tc_inst_data_i & (MASK_C_JR | MASK_RD)) ==
                    (MATCH_C_JR | (X_RA << OP_SH_RD));
 
         // non mandatory mode
-        /*if(trace_implicit_ret)
+        /*if(implicit_return_i)
             not_ret = !(is_ret || is_c_ret);*/
 
         updiscon_o = (is_jump || is_priv_ret) && not_ret;
