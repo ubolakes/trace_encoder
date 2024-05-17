6db6fec7b0f0b467048ae88e8adb11edfc77a9da
Umberto Laghi <ul2509@gmail.com>
2024-05-17 18:12:06 +0200

    added signals length

--- tb/tb_trdb_packet_emitter.sv
+++ tb/tb_trdb_packet_emitter.sv
@@ -4,6 +4,8 @@
 
 `timescale 1ns/1ns
 
+import trdb_pkg::*;
+
 module tb_trdb_packet_emitter();
 
     logic clk;
@@ -14,10 +16,10 @@ module tb_trdb_packet_emitter();
     logic [1:0]     packet_format_i;
     logic [1:0]     packet_subformat_i;
     logic [4:0]     lc_cause_i;
-    logic [X:0]     lc_tval_i;
+    logic [31:0]    lc_tval_i;
     logic           lc_interrupt_i;
     logic [4:0]     tc_cause_i;
-    logic [X:0]     tc_tval_i;
+    logic [31:0]    tc_tval_i;
     logic           tc_interrupt_i;
     logic           nocontext_i;
     logic           notime_i;
@@ -26,27 +28,27 @@ module tb_trdb_packet_emitter();
     logic [1:0]     priv_i;
     logic [31:0]    iaddr_i;
     logic           lc_tc_mux_i;
-    logic [X:0]     thaddr_i;
-    logic [X:0]     tvec_i;
-    logic [X:0]     lc_epc_i;
+    logic           thaddr_i;
+    logic [31:2]    tvec_i;
+    logic [31:0]    lc_epc_i;
     logic           ienable_i;
     logic           encoder_mode_i;
-    logic           qual_status_i;
-    logic           delta_address_i;
+    logic [1:0]     qual_status_i;
+    logic [2:0]     ioptions_i;
     logic           lc_updiscon_i;
-    logic           branches_i;
-    logic           branch_map_i;    
+    logic [4:0]     branches_i;
+    logic [30:0]    branch_map_i;    
 
     // outputs
     logic           packet_valid_o;
-    logic [:0]      packet_payload_o;
-    logic [:0]      payload_length_o;
+    logic [255:0]   packet_payload_o;
+    logic [4:0]     payload_length_o;
     logic           branch_map_flush_o;
 
     // testing only output
     logic           expected_packet_valid;
-    logic [:0]      expected_packet_payload;
-    logic [:0]      expected_payload_length;
+    logic [255:0]   expected_packet_payload;
+    logic [4:0]     expected_payload_length;
     logic           expected_branch_map_flush;
 
     // iteration variable
@@ -78,7 +80,7 @@ module tb_trdb_packet_emitter();
         .ienable_i(ienable_i),
         .encoder_mode_i(encoder_mode_i),
         .qual_status_i(qual_status_i),
-        .delta_address_i(delta_address_i),
+        .ioptions_i(ioptions_i),
         .lc_updiscon_i(lc_updiscon_i),
         .branches_i(branches_i),
         .branch_map_i(branch_map_i),
@@ -99,18 +101,44 @@ module tb_trdb_packet_emitter();
     end
 
     always @(posedge clk) begin
-        {valid_i, packet_format_i, packet_subformat_i, lc_cause_i, lc_tval_i, lc_interrupt_i, tc_cause_i,
-        tc_tval_i, tc_interrupt_i, nocontext_i, notime_i, tc_branch_i, tc_branch_taken_i, priv_i, iaddr_i,
-        lc_tc_mux_i, thaddr_i, tvec_i, lc_epc_i, ienable_i, encoder_mode_i, qual_status_i, delta_address_i,
-        lc_updiscon_i, branches_i, branch_map_i, expected_packet_valid, expected_packet_payload,
-        expected_payload_length, expected_branch_map_flush} = test_vector[i]; #10;
+        {valid_i,
+        packet_format_i,
+        packet_subformat_i,
+        lc_cause_i,
+        lc_tval_i,
+        lc_interrupt_i,
+        tc_cause_i,
+        tc_tval_i,
+        tc_interrupt_i,
+        nocontext_i,
+        notime_i,
+        tc_branch_i,
+        tc_branch_taken_i,
+        priv_i,
+        iaddr_i,
+        lc_tc_mux_i,
+        thaddr_i,
+        tvec_i,
+        lc_epc_i,
+        ienable_i,
+        encoder_mode_i,
+        qual_status_i,
+        ioptions_i,
+        lc_updiscon_i,
+        branches_i,
+        branch_map_i,
+        expected_packet_valid,
+        expected_packet_payload,
+        expected_payload_length,
+        expected_branch_map_flush
+        } = test_vector[i]; #10;
         
     end
 
-    always_ff @(negedge clk) begin
+    always_ff @(negedge clk) begin // prints the line if it's wrong
         // packet_valid_o
         if(expected_packet_valid !== packet_valid_o) begin
-            $display("Wrong valid: %b!=%b", expected_packet_valid, packet_valid_o); // printed if it's wrong
+            $display("Wrong valid: %b!=%b", expected_packet_valid, packet_valid_o);
         end
         // packet_payload_o
         if(expected_packet_payload !== packet_payload_o) begin
