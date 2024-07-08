60f03a91581b17f5aceab62ef10dc5cad5a24d66
Umberto Laghi <ul2509@gmail.com>
2024-07-08 13:26:46 +0200

    implemented new filter more similar to the spec

--- rtl/trdb_filter.sv
+++ rtl/trdb_filter.sv
@@ -4,61 +4,130 @@
 
 /* FILTER MODULE */
 /*
-it enables and disables the trace encoder
+it declares instructions qualified or not
 */
 
 import trdb_pkg::*;
 
 module trdb_filter
 (
-    // setting input values are taken from registers
-    
-    // uncommented signals are the ones required to simply turn on and of the encoder
-
-    // enable/disable tracing
-    input logic trace_enable_i, // read from registers
-    output logic trace_req_deactivate_o, // writes to register
-
-    // trigger unit signals
-    // this value influences the trace_enable signal
-    input logic trigger_trace_off_i,
-    
     /*
-    // consider/ignore filters
-    input logic apply_filters_i,
+    The idea is to have all inputs to be filtered in range or match.
+    To achieve this, the filter requires for each parameter:
+        - enable
+        - upper and lower values
+        - to be matched value
+        - select between in range or match
+        - qualified for each mode
 
-    // tracing a specific privilege mode
-    input logic trace_selected_priv_i, // on/off
-    input logic [1:0] which_priv_i, // selected priv
-    input logic [1:0] priv_lvl_i, // input priv
-
-    // trace specific address range
-    input logic trace_range_event_i,
-    input logic trace_stop_event_i,
-    input logic [XLEN-1:0] trace_lower_addr_i,
-    input logic [XLEN-1:0] trace_higher_addr_i
-    input logic [XLEN-1:0] iaddr_i
-    
-    // signals to determine matches
-    output logic trace_range_match_o,
-    output logic trace_priv_match_o,
+    The trace to be qualified according to the filters set it requires 
+    an AND between the qualified of the parameters to filter.
     */
-    output logic nc_trace_qualified_o
+
+    // parameters to filter
+    // cause
+    input logic                 cause_filter_i,
+    input logic [CAUSE_LEN-1:0] upper_cause_i,
+    input logic [CAUSE_LEN-1:0] lower_cause_i,
+    input logic [CAUSE_LEN-1:0] match_cause_i,
+    input logic                 cause_range_mode_i,
+    input logic                 cause_equal_mode_i,
+    input logic [CAUSE_LEN-1:0] cause_i,
+    // tvec
+    input logic                 tvec_filter_i,
+    input logic [XLEN-1:2]      upper_tvec_i,
+    input logic [XLEN-1:2]      lower_tvec_i,
+    input logic [XLEN-1:2]      match_tvec_i,
+    input logic                 tvec_range_mode_i,
+    input logic                 tvec_equal_mode_i,
+    input logic [XLEN-1:2]      tvec_i,
+    // tval
+    input logic                 tval_filter_i,
+    input logic [XLEN-1:0]      upper_tval_i,
+    input logic [XLEN-1:0]      lower_tval_i,
+    input logic [XLEN-1:0]      match_tval_i,
+    input logic                 tval_range_mode_i,
+    input logic                 tval_equal_mode_i,
+    input logic [XLEN-1:0]      tval_i,
+    // priv_lvl
+    input logic                 priv_lvl_filter_i,
+    input logic [XLEN-1:2]      upper_priv_lvl_i,
+    input logic [XLEN-1:2]      lower_priv_lvl_i,
+    input logic [XLEN-1:2]      match_priv_lvl_i,
+    input logic                 priv_lvl_range_mode_i,
+    input logic                 priv_lvl_equal_mode_i,
+    input logic [XLEN-1:2]      priv_lvl_i,
+    // iaddr (pc)
+    input logic                 iaddr_filter_i,
+    input logic [XLEN-1:2]      upper_iaddr_i,
+    input logic [XLEN-1:2]      lower_iaddr_i,
+    input logic [XLEN-1:2]      match_iaddr_i,
+    input logic                 iaddr_range_mode_i,
+    input logic                 iaddr_equal_mode_i,
+    input logic [XLEN-1:2]      iaddr_i,
+
+    output logic nc_qualified_o
 );
 
-    assign trace_req_deactivate_o = trigger_trace_off_i;
-    assign nc_trace_qualified_o = trace_enable_i ? 1 : 0;
+    // output signals for comparators
+    // cause
+    logic cause_in_range;
+    logic cause_equals;
+    // tvec
+    logic tvec_in_range;
+    logic tvec_equals;
+    // tval
+    logic tval_in_range;
+    logic tval_equals;
+    // priv_lvl
+    logic priv_lvl_in_range;
+    logic priv_lvl_equals;
+    // iaddr (pc)
+    logic iaddr_in_range;
+    logic iaddr_equals;
 
-    /* to be used in case of more complex filtering
-    always_comb begin
-        nc_trace_qualified_o = '0;
-        // tracing
-        if(!apply_filters_i)
-            nc_trace_qualified_o = trace_enable_i;
-        else
-            nc_trace_qualified_o = trace_enable_i;
-                                //&& (trace_range_event_i && ip_in_range) && priv_matching;
-    end
-    */
+    // assignments
+    // cause
+    assign cause_in_range = cause_range_mode_i &&
+                            cause_i < upper_cause_i &&
+                            cause_i > lower_cause_i;
+    assign cause_equals =   cause_equal_mode_i &&
+                            cause_i == match_cause_i;
+    // tvec
+    assign tvec_in_range =  tvec_range_mode_i &&
+                            tvec_i < upper_tvec_i &&
+                            tvec_i > lower_tvec_i;
+    assign tvec_equals =    tvec_equal_mode_i &&
+                            tvec_i == match_tvec_i;
+    // tval
+    assign tval_in_range =  tval_range_mode_i &&
+                            tval_i < upper_tval_i &&
+                            tval_i > lower_tval_i;
+    assign tval_equals =    tval_equal_mode_i &&
+                            tval_i == match_tval_i;
+    // priv_lvl
+    assign priv_lvl_in_range =  priv_lvl_range_mode_i &&
+                                priv_lvl_i < upper_priv_lvl_i &&
+                                priv_lvl_i > lower_priv_lvl_i;
+    assign priv_lvl_equals =    priv_lvl_equal_mode_i &&
+                                priv_lvl_i == match_priv_lvl_i;
+    // iaddr
+    assign iaddr_in_range = iaddr_range_mode_i &&
+                            iaddr_i < upper_iaddr_i &&
+                            iaddr_i > lower_iaddr_i;
+    assign iaddr_equals =   iaddr_equal_mode_i &&
+                            iaddr_i == match_iaddr_i;
+
+    // assigning output
+    assign nc_qualified_o = ((cause_filter_i && (cause_in_range || cause_equals)) ||
+                            ~cause_filter_i) && // cause
+                            ((tvec_filter_i && (tvec_in_range || tvec_equals)) ||
+                            ~tvec_filter_i) && // tvec
+                            ((tval_filter_i && (tval_in_range || tval_equals)) ||
+                            ~tval_filter_i) && // tval
+                            ((priv_lvl_filter_i && (priv_lvl_in_range || priv_lvl_equals)) ||
+                            ~priv_lvl_filter_i) && // priv_lvl
+                            ((iaddr_filter_i && (iaddr_in_range || iaddr_equals)) ||
+                            ~iaddr_filter_i); // iaddr
 
 endmodule
