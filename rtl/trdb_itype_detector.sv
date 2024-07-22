6c66c2c04a2d3ff7a619bf7ceb4771a66cbb4e74
Umberto Laghi <ul2509@gmail.com>
2024-07-22 18:15:53 +0200

    minor edit

--- rtl/trdb_itype_detector.sv
+++ rtl/trdb_itype_detector.sv
@@ -45,7 +45,7 @@ module trdb_itype_detector
                             ((nc_inst_data_i & MASK_C_BNEZ)   == MATCH_C_BNEZ))/* && 
                             nc_ready_i && !same_instr*/;
     assign tc_branch_taken_o = tc_compressed_i ?
-                                !(tc_iaddr_i + 2 == nc_iaddr_i);// && both_ready && !same_instr :
+                                !(tc_iaddr_i + 2 == nc_iaddr_i):// && both_ready && !same_instr :
                                 !(tc_iaddr_i + 4 == nc_iaddr_i);// && both_ready && !same_instr;
 
     // compressed inst - not supported by snitch
