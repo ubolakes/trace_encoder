e8c29534e491cb6dad9dc8966a90b8564044efe9
Umberto Laghi <ul2509@gmail.com>
2024-08-09 16:21:17 +0200

    updated testbench for tb_trdb_itype_detector

--- tb/tb_trdb_itype_detector.sv
+++ tb/tb_trdb_itype_detector.sv
@@ -12,40 +12,48 @@ module tb_trdb_itype_detector();
     logic reset;
 
     // inputs
-    logic valid_i;
-    logic [XLEN-1:0]    nc_inst_data_i;
-    logic               tc_compressed_i;
+    logic               lc_valid_i;
+    logic               tc_valid_i;
+    logic               nc_valid_i;
+    logic [XLEN-1:0]    lc_iaddr_i;
     logic [XLEN-1:0]    tc_iaddr_i;
     logic [XLEN-1:0]    nc_iaddr_i;
-    logic               nc_exception_i;
+    logic [XLEN-1:0]    tc_inst_data_i;
+    logic               tc_compressed_i;
+    logic               tc_exception_i;
 
     // outputs
-    logic nc_branch_o;
+    logic tc_branch_o;
     logic tc_branch_taken_o;
-    logic nc_updiscon_o;
+    logic tc_updiscon_o;
 
     // testing only outputs
-    logic expected_nc_branch;
+    logic expected_tc_branch;
     logic expected_tc_branch_taken;
-    logic expected_nc_updiscon;
+    logic expected_tc_updiscon;
 
     // iteration variable
     logic [31:0] i;
 
     // DUT instantiation
     trdb_itype_detector DUT(
-        .valid_i          (valid_i),
-        .nc_inst_data_i   (nc_inst_data_i),
-        .tc_compressed_i  (tc_compressed_i),
+        .clk_i            (clk),
+        .rst_ni           (reset),
+        .lc_valid_i       (lc_valid_i),
+        .tc_valid_i       (tc_valid_i),
+        .nc_valid_i       (nc_valid_i),
+        .lc_iaddr_i       (lc_iaddr_i),
         .tc_iaddr_i       (tc_iaddr_i),
         .nc_iaddr_i       (nc_iaddr_i),
-        .nc_exception_i   (nc_exception_i),
-        .nc_branch_o      (nc_branch_o),
+        .tc_inst_data_i   (tc_inst_data_i),
+        .tc_compressed_i  (tc_compressed_i),
+        .tc_exception_i   (tc_exception_i),
+        .tc_branch_o      (tc_branch_o),
         .tc_branch_taken_o(tc_branch_taken_o),
-        .nc_updiscon_o    (nc_updiscon_o)
+        .tc_updiscon_o    (tc_updiscon_o)
     );
 
-    logic [101:0] test_vector[1000:0];
+    logic [135:0] test_vector[1000:0];
     //    length of line   # of lines
 
     initial begin // reading test vector
@@ -56,30 +64,33 @@ module tb_trdb_itype_detector();
     end
 
     always @(posedge clk) begin // on posedge we get expected output
-        {valid_i,
-        nc_inst_data_i,
-        tc_compressed_i,
+        {lc_valid_i,
+        tc_valid_i,
+        nc_valid_i,
+        lc_iaddr_i,
         tc_iaddr_i,
         nc_iaddr_i,
-        nc_exception_i,
-        expected_nc_branch,
+        tc_inst_data_i,
+        tc_compressed_i,
+        tc_exception_i,
+        expected_tc_branch,
         expected_tc_branch_taken,
-        expected_nc_updiscon
+        expected_tc_updiscon
         } = test_vector[i]; #10; 
     end
 
     always @(negedge clk) begin// on negedge we compare the expected result with the actual one
         // nc_branch_o
-        if(expected_nc_branch !== nc_branch_o) begin
-            $display("Wrong nc_branch: %b!=%b", expected_nc_branch, nc_branch_o); // printed if it's wrong
+        if(expected_tc_branch !== tc_branch_o) begin
+            $display("Wrong tc_branch: %b!=%b", expected_tc_branch, tc_branch_o); // printed if it's wrong
         end        
         // tc_branch_taken_o
         if(expected_tc_branch_taken !== tc_branch_taken_o) begin
             $display("Wrong tc_branch_taken: %b!=%b", expected_tc_branch_taken, tc_branch_taken_o);
         end
         // nc_updiscon_o
-        if(expected_nc_updiscon !== nc_updiscon_o) begin
-            $display("Wrong nc_updiscon: %b!=%b", expected_nc_updiscon, nc_updiscon_o);
+        if(expected_tc_updiscon !== tc_updiscon_o) begin
+            $display("Wrong nc_updiscon: %b!=%b", expected_tc_updiscon, tc_updiscon_o);
         end
 
         // index increase
